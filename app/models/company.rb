bcc6d5d2ff1b2301c2f210c708a03f5606f90ada
PopescuGheorghe <gimmy.xdd@gmail.com>
2016-03-13 15:34:44 +0200

    ui updates

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,23 @@
 class Company < ActiveRecord::Base
   has_many :invoices, dependent: :destroy
   # Customer association
-  has_many :customer_invoices, foreign_key: "customer_id", class_name: "Invoice", dependent: :destroy
+  has_many :customer_invoices, foreign_key: 'customer_id', class_name: 'Invoice', dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
+
+  def to_receive
+    @total = 0
+    self.invoices.each do |invoice|
+      @total += invoice.total
+    end
+    @total
+  end
+
+  def to_pay
+    @total = 0
+    self.customer_invoices.each do |invoice|
+      @total += invoice.total
+    end
+    @total
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,8 @@ class Item < ActiveRecord::Base
   validates :description, presence: true
   validates :price, numericality: true
   validates :quantity, numericality: { only_integer: true }
+
+  def total_price
+    self.price * self.quantity
+  end
 end
